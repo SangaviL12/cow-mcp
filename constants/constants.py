4a66deb571a03a0c889c5787f62adc1acdc8b69d
Sangavi <sangavi.l@continube.com>
2025-08-08 22:18:05 +0530

    api added

--- constants/constants.py
+++ constants/constants.py
@@ -77,7 +77,8 @@ URL_WORKFLOW_RESOURCE_DATA = "/v2/aggregator/fetch-resource-data"
 URL_FETCH_RULES = "/pc-api/v1/rules"
 URL_FETCH_TASKS = "/pc-api/v1/tasks"
 URL_CREATE_RULE = "/pc-api/v2/rules"
-
+URL_EXECUTE_RULE = "/pc-api/v2/rules/execute-rule"
+URL_FETCH_EXECUTION_PROGRESS = "/pc-api/v2/rules/fetch-execution-progress"
 
 # STORAGE
 URL_UPLOAD_FILE = "/pc-api/v1/storage/upload-file"
@@ -88,4 +89,8 @@ URL_FETCH_DESIGN_NOTES = "/pc-api/v1/design-notes/fetch-file"
 
 # README
 URL_SAVE_RULE_README = "/pc-api/v2/rules/upsert-readme"
+
+# CREDENTIAL
+URL_FETCH_CREDENTIAL = "/v1/credential"
+URL_FETCH_APPLICATION_CREDENTIALS = "/pc-api/v1/application-credentials"
 
